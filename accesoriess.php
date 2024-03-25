51ad6b6098bf2b7072866015ff45eec1a7490739
surinduRansith <surindu.ransith@gmail.com>
2024-03-25 22:06:21 +0530

    iconupdate

--- accesoriess.php
+++ accesoriess.php
@@ -218,7 +218,7 @@ if(isset($_POST['clearInvoice'])){
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+<link rel="shortcut icon" type="x-icon" href="assets/images/sdlogo.jpeg">
 <link rel="stylesheet" href="style.css">
     <title>Stocks</title>
 </head>
