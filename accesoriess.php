c017f3dc9e98182e87eca10f3bf705f5b7c1df27
surinduRansith <surindu.ransith@gmail.com>
2024-04-25 14:00:25 +0530

    uichange

--- accesoriess.php
+++ accesoriess.php
@@ -310,7 +310,10 @@ echo "<tbody>";
         <td>".$value['qty']."</td>
         <td> 
         <button type='submit' name='item' class='btn btn-warning' value='".$index1."'>
-        add to cart
+        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'  width='16' height='16' fill='currentColor'  stroke-width='1.5' stroke='currentColor' class='w-6 h-6'>
+  <path stroke-linecap='round' stroke-linejoin='round' d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z' />
+</svg>
+
         </button>  
         </td>
     </tr>
@@ -344,7 +347,7 @@ echo "<tbody>";
         <?php 
 if(!empty($cartList)){
 ?>
-        <div class="container text-center">
+        <div>
             <form method="post">
 
                 <div class="row align-items-start">
@@ -394,10 +397,12 @@ $notetest="-";
         <td>".$stockArray[$index]['Type']."</td>
         <td>Rs. ".$stockArray[$index]['SellingPrice']."</td>
         <td><input type='number' name='quantity[$index]' value='1'></td>
-        <td><input type='text' name='note[$index]' value='".$notetest."'></td>
+        <td><input type='text'  name='note[$index]' value='".$notetest."'></td>
         <td>
         <button type='submit' name='itemremove' class='btn btn-danger' value='".$index."'>
-        Remove
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='16' height='16' fill='currentColor' class='w-6 h-6'>
+        <path fill-rule='evenodd' d='M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z' clip-rule='evenodd' />
+      </svg>
         </button>
         </td>
 
@@ -445,7 +450,7 @@ $notetest="-";
     echo "</table>";
     
     ?>
-
+<div class="container text-center">
                         <button type="submit" name="itemadd" class="btn btn-success"> Add Bill </button>
 
                         <?php  
@@ -453,7 +458,7 @@ $notetest="-";
 echo $downloadInvoice;
     ?>
                         <button type='submit' name='clearInvoice' class='btn btn-danger'> Clear Invoice</button>
-
+</div>
             </form>
             <?php
 }
